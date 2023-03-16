54da76f73a09808e1876b2c1bfb5fd5048557ebf
Anton Charny <anton.charny@perimeter81.com>
2023-03-16 11:50:10 +0200

    Initial Commit

---
+++ SwiftMarathon6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftMarathon6
+//
+//  Created by Anton Charny on 16/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
